e4109f30c8238f175b7bd2db78967ce6a26bb729
Btran1291 <khoit12@outlook.com>
2025-01-24 08:35:46 -0600

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from brave_search import brave_search_bp
 from vectorize_query import vectorize_query_bp
+from generate_docx import generate_docx_bp
 
 app = Flask(__name__)
 
-# Register the brave_search app blueprint
 app.register_blueprint(brave_search_bp, url_prefix='/')
-
-# Register the vectorize_query app blueprint
 app.register_blueprint(vectorize_query_bp, url_prefix='/')
+app.register_blueprint(generate_docx_bp, url_prefix='/')
