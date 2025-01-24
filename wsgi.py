be8ae4b328ae1abe7f8fcac81489dbaf7a44405d
Btran1291 <khoit12@outlook.com>
2025-01-24 00:32:09 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,18 @@
 import sys
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.wrappers import Response
+from flask_cors import CORS
 
-# Add your project directory to the sys.path
 project_home = '.'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
-# Import the brave_search app
 from brave_search import app as brave_search_app
-
-# Import the vectorize_rag app
 from vectorize_query import app as vectorize_rag_app
 
-# Create a dispatcher middleware to route requests
+CORS(brave_search_app, resources={r"/*": {"origins": "*"}})
+CORS(vectorize_rag_app, resources={r"/*": {"origins": "*"}})
+
 application = DispatcherMiddleware(Response('Not Found', status=404), {
     '/brave_search': brave_search_app,
     '/vectorize-rag-retrieve': vectorize_rag_app
